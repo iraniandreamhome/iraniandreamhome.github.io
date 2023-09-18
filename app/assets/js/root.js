fd68ad0616dbe1fb0ef7d883748d89ad007ba974
aarsalannazari <aarsalannazari@gmail.com>
2023-09-19 03:21:34 +0330

    change some big things

--- app/assets/js/root.js
+++ app/assets/js/root.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-    $("#loading").hide();
-    $("#bg-loading").hide();
-    $("#loading-text").hide();
-})
+$(document).ready(function () {
+  $(".loading").hide();
+  $(".bg-loading").hide();
+  $(".loading-text").hide();
+});
 
 function more_social() {
+  Swal.fire({
+    title: "خطا",
+    text: "این گزینه فعلا فعال نیست !",
+    icon: "error",
+    confirmButtonText: "باشه",
+    confirmButtonColor: "#00c3ff",
+    showCloseButton: true
+  });
 }
